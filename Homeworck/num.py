34317825f7a52f16bc0b7640de7e571078a9cb04
DmitriyKomarovCoder <66564922+DmitriyKomarovCoder@users.noreply.github.com>
2022-12-04 22:04:29 +0300

    Homeworck

---
+++ Homeworck/num.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from fib import fib
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_word():
+    return "<p>Returning the Fibonacci numbers!</p>"
+
+@app.route('/num/<int:cnt>')
+def get_fib(cnt):
+    fib_gen = fib()
+    res = [next(fib_gen) for _ in range(cnt)]
+    return res
